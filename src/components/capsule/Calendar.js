68339d0438c2655cc64149936be38e9c44166a1a
Abdul Mannan <abdulmannankhan1000@gmail.com>
2023-07-19 11:48:08 +0500

    issue fixed

--- src/components/capsule/Calendar.js
+++ src/components/capsule/Calendar.js
@@ -381,13 +381,13 @@ function WeekScheduler() {
                     </div>
                     <div className="mt-4">
                         <button type="button"
-                                disabled={event.status === 'Canceled'}
+                                disabled={event.status === 'Canceled' || event.status === 'Completed'}
                                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:opacity-30"
                                 onClick={() => updateEvent(event)}
                         >Completed
                         </button>
                         <button type="button"
-                                disabled={event.status === 'Completed'}
+                                disabled={event.status === 'Completed' || event.status === 'Canceled'}
                                 className=":outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 disabled:opacity-30"
                                 onClick={() => cancelEvent(event)}
                         >Cancel
